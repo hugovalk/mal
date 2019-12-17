7431b5191b9e9ffb63c92859dbf91f28561410f1
Hugo Valk <hugo.valk@gmail.com>
2019-12-17 23:33:25 +0100

    Restructure Reader.

--- scaladd/src/main/scala/mal/Reader.scala
+++ scaladd/src/main/scala/mal/Reader.scala
@@ -18,17 +18,32 @@ object Reader {
   def tokenize(input: String): List[Token] =
     MAL_PATTERN.findAllMatchIn(input).toList.map(_.group(1)).filter(t => t != "" && !t.startsWith(";"))
 
-  def readSeq(result: MalSeq, tokens: List[Token], endToken: String): (MalSeq, List[Token]) = {
+  def readForm(tokens: List[Token]): (MalType, List[Token]) = {
+    tokens match {
+      case t :: ts if t == "(" => buildSequence(MalList(), ts, ")")
+      case t :: ts if t == "[" => buildSequence(MalVec(), ts, "]")
+      case t :: ts if t == "{" => buildMap(MalMap(), ts)
+      case t :: ts if t == "'" => buildQuoted(ts, MalSymbol("quote"))
+      case t :: ts if t == "`" => buildQuoted(ts, MalSymbol("quasiquote"))
+      case t :: ts if t == "~" => buildQuoted(ts, MalSymbol("unquote"))
+      case t :: ts if t == "~@" => buildQuoted(ts, MalSymbol("splice-unquote"))
+      case t :: ts if t == "@" => buildQuoted(ts, MalSymbol("deref"))
+      case t :: ts if t == "^" => buildWithMeta(ts)
+      case _ => buildAtom(tokens)
+    }
+  }
+
+  def buildSequence(result: MalSeq, tokens: List[Token], endToken: Token): (MalSeq, List[Token]) = {
     tokens match {
       case t :: ts if t == endToken => (result, ts)
       case t :: _ if t != endToken =>
         val (form, remainingTokens) = readForm(tokens)
-        readSeq(result.append(form), remainingTokens, endToken)
+        buildSequence(result.append(form), remainingTokens, endToken)
       case Nil => throw new IllegalStateException(".*(EOF|end of input|unbalanced).*")
     }
   }
 
-  def readMap(result: MalMap, tokens: List[Token]): (MalMap, List[Token]) = {
+  def buildMap(result: MalMap, tokens: List[Token]): (MalMap, List[Token]) = {
     tokens match {
       case t :: ts if t == "}" => (result, ts)
       case t :: _ if t != "}" =>
@@ -36,58 +51,44 @@ object Reader {
         remaining1 match {
           case t2 :: ts2 if t2 != "}" =>
             val (form2, remaining2) = readForm(remaining1)
-            readMap(result.+((form1, form2)), remaining2)
+            buildMap(result.+((form1, form2)), remaining2)
           case _ => throw new IllegalStateException(".*(EOF|end of input|unbalanced).*")
         }
       case Nil => throw new IllegalStateException(".*(EOF|end of input|unbalanced).*")
     }
   }
 
-  def readAtom(tokens: List[Token]): (MalType, List[Token]) = tokens match {
-    case t :: ts => (MalSymbol(t), ts)
-    case Nil => throw new IllegalStateException("No atom in empty token list.")
-  }
-
-  def quoting(tokens: List[Token], quoteSymol: MalSymbol): (MalType, List[Token]) = {
+  def buildQuoted(tokens: List[Token], quoteSymol: MalSymbol): (MalType, List[Token]) = {
     val (result, remaining) = readForm(tokens)
     (MalList(Vector(quoteSymol, result)), remaining)
   }
-  def meta(tokens: List[Token]): (MalType, List[Token]) = {
+
+  def buildWithMeta(tokens: List[Token]): (MalType, List[Token]) = {
     val (meta, remaining) = readForm(tokens)
     val (result, finalRemaining) = readForm(remaining)
     (MalList(MalSymbol("with-meta"), result, meta), finalRemaining)
   }
 
-  def invalidString(token: String): Boolean = {
-    token == "\"" ||
-      !token.endsWith("\"") ||
-      !"^.*?([\\\\]*)\"$".r.findFirstMatchIn(token).forall(_.group(1).length % 2 == 0)
-  }
+  def buildString(token: Token): MalString = {
+    def invalidString(token: String): Boolean = {
+      token == "\"" ||
+        !token.endsWith("\"") ||
+        !"^.*?([\\\\]*)\"$".r.findFirstMatchIn(token).forall(_.group(1).length % 2 == 0)
+    }
 
-  def readStr(token: Token): MalString = {
     if (invalidString(token))
       throw new IllegalStateException(".*(EOF|end of input|unbalanced).*")
     else
       MalString(token)
   }
 
-  def readNumber(token: Token): MalNumber =
+  def buildNumber(token: Token): MalNumber =
     MalNumber(BigDecimal(token))
 
-  def readForm(tokens: List[Token]): (MalType, List[Token]) = {
-    tokens match {
-      case t :: ts if t == "(" => readSeq(MalList(), ts, ")")
-      case t :: ts if t == "[" => readSeq(MalVec(), ts, "]")
-      case t :: ts if t == "'" => quoting(ts, MalSymbol("quote"))
-      case t :: ts if t == "`" => quoting(ts, MalSymbol("quasiquote"))
-      case t :: ts if t == "~" => quoting(ts, MalSymbol("unquote"))
-      case t :: ts if t == "~@" => quoting(ts, MalSymbol("splice-unquote"))
-      case t :: ts if t == "@" => quoting(ts, MalSymbol("deref"))
-      case t :: ts if t == "^" => meta(ts)
-      case t :: ts if t == "{" => readMap(MalMap(), ts)
-      case t :: ts if t.startsWith("\"") => (readStr(t), ts)
-      case t :: ts if t.matches("[0-9.]+") => (readNumber(t), ts)
-      case _ => readAtom(tokens)
-    }
+  def buildAtom(tokens: List[Token]): (MalType, List[Token]) = tokens match {
+    case t :: ts if t.startsWith("\"") => (buildString(t), ts)
+    case t :: ts if t.matches("[0-9.]+") => (buildNumber(t), ts)
+    case t :: ts => (MalSymbol(t), ts)
+    case Nil => throw new IllegalStateException("No atom in empty token list.")
   }
 }
